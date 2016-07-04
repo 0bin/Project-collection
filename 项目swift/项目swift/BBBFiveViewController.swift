f5702889f4518b82998c641fd928c7d673c9f034
linbin <149723722@qq.com>
2016-07-04 16:48:31 +0800

    添加中间tabbar按钮

---
+++ 项目swift/项目swift/BBBFiveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BBBFiveViewController.swift
+//  项目swift
+//
+//  Created by LinBin on 16/7/4.
+//  Copyright © 2016年 LinBin. All rights reserved.
+//
+
+import UIKit
+
+class BBBFiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.grayColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 项目swift/项目swift/BBBTabBarController.swift
+++ 项目swift/项目swift/BBBTabBarController.swift
@@ -9,31 +9,75 @@
 import UIKit
 
 class BBBTabBarController: UITabBarController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBar.tintColor = UIColor.orangeColor()
+        addChildVC()
         
         
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addComposeButton()
+        
+        
+    }
+    
+    
+    /**
+     添加tabBar中间button
+     */
+    func addComposeButton() {
+        let ComposeButton = UIButton()
+        ComposeButton.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        ComposeButton.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        ComposeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        ComposeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
+        ComposeButton.addTarget(self, action: #selector(composeButtonClick), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        let buttonW = UIScreen.mainScreen().bounds.size.width / CGFloat(viewControllers!.count)
+        let buttonH = tabBar.bounds.size.height
+        let buttonX = tabBar.bounds.size.width * 0.5
+        let buttonY = tabBar.bounds.size.height * 0.5
+        ComposeButton.frame = CGRectMake(0, 0, buttonW, buttonH)
+        ComposeButton.center = CGPointMake(buttonX, buttonY)
+        
+        tabBar.addSubview(ComposeButton)
+    }
+    
+    /**
+     中间button的点击方法
+     */
+    func composeButtonClick()  {
+        let fiveVC = BBBFiveViewController()
+        self.presentViewController(fiveVC, animated: true, completion: nil)
+        
+        print(#function)
+    }
+
+    /**
+     添加tabbar子视图
+     */
+    func addChildVC() {
         let oneVC = BBBOneViewController()
         self.addChildVC(oneVC, title: "One", imageName: "tabbar_home", highlightedImageName: "tabbar_home_highlighted")
         
         let twoVC = BBBTwoViewController()
         self.addChildVC(twoVC, title: "Two", imageName: "tabbar_home", highlightedImageName: "tabbar_home_highlighted")
         
+        let fiveVC = BBBFiveViewController()
+        self.addChildVC(fiveVC, title: "", imageName: "", highlightedImageName: "")
+        
         let threeVC = BBBThreeController()
         self.addChildVC(threeVC, title: "Three", imageName: "tabbar_home", highlightedImageName: "tabbar_home_highlighted")
         
         let fourVC = BBBFourController()
         self.addChildVC(fourVC, title: "Four", imageName: "tabbar_home", highlightedImageName: "tabbar_home_highlighted")
-        
-        
-
-   
-        
-
-        
     }
     
     /**
@@ -48,7 +92,7 @@ class BBBTabBarController: UITabBarController {
                     title:String,
                     imageName:String,
                     highlightedImageName:String){
-        //创建并设置第一个页面
+        //设置页面
         childVC.title = title
         childVC.tabBarItem.image = UIImage(named: imageName)
         childVC.tabBarItem.selectedImage = UIImage(named: highlightedImageName)
