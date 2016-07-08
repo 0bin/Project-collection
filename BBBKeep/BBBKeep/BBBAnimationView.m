6f7c3bebae5edfe7646e1c16cfa5672491066550
linbin <149723722@qq.com>
2016-07-08 09:22:56 +0800

    添加文字

--- BBBKeep/BBBKeep/BBBAnimationView.m
+++ BBBKeep/BBBKeep/BBBAnimationView.m
@@ -26,8 +26,35 @@
 - (void)awakeFromNib
 {
     self.backgroundColor = [UIColor clearColor];
+    [self addLabel];
     
     
+
+}
+
+- (void)addLabel
+{
+    NSArray *array = @[@"全程记录你的健身数据",@"规范你的训练过程",@"陪伴你迈出跑步的第一步",@"分享汗水后你的性感"];
+    
+    CGFloat labelW = self.scrollView.frame.size.width;
+    CGFloat labelH = self.scrollView.frame.size.height;
+    CGFloat labelY = 0;
+    
+    for (NSInteger i = 0; i < array.count; i++) {
+        
+        UILabel *label = [[UILabel alloc] init];
+        label.frame = CGRectMake(labelW * i, labelY, labelW, labelH);
+        label.text = array[i];
+        label.textColor = [UIColor whiteColor];
+        [label setTextAlignment:NSTextAlignmentCenter];
+        [self.scrollView addSubview:label];
+        
+        
+    }
+    
+    [self.scrollView setContentSize:CGSizeMake(labelW * array.count, 0)];
+    [self.scrollView setPagingEnabled:YES];
+   
 
 }
 /*
